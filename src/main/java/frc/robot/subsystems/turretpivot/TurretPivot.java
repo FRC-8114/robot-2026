52042038156752422be54c618cd54b32e582032c
Collin Williams <96917990+bluedragon1221@users.noreply.github.com>
2026-02-09 17:40:57 -0600

    ditch IOs and rewrite some parts

--- src/main/java/frc/robot/subsystems/turretpivot/TurretPivot.java
+++ src/main/java/frc/robot/subsystems/turretpivot/TurretPivot.java
@@ -5,16 +5,41 @@ import static frc.robot.subsystems.turretpivot.TurretPivotConstants.*;
 
 import java.util.function.Supplier;
 
+import com.ctre.phoenix6.CANBus;
+import com.ctre.phoenix6.StatusSignal;
+import com.ctre.phoenix6.controls.MotionMagicVoltage;
+import com.ctre.phoenix6.hardware.CANcoder;
+import com.ctre.phoenix6.hardware.TalonFX;
+
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.units.measure.Angle;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class TurretPivot extends SubsystemBase {
-    private TurretPivotIO io;
+    private final TalonFX pivotMotor = new TalonFX(pivotMotorID, new CANBus("canivore"));
+    private final CANcoder pivotEncoder = new CANcoder(pivotEncoderID);
 
-    TurretPivot(TurretPivotIO io) {
-        this.io = io;
+    private final MotionMagicVoltage control = new MotionMagicVoltage(0);
+    private final StatusSignal<Angle> pivotAngle = pivotMotor.getPosition();
+
+    TurretPivot() {
+        pivotMotor.getConfigurator().apply(pivotMotorCfg);
+        pivotEncoder.getConfigurator().apply(pivotEncoderCfg);
+    }
+
+    private Command setPosition(Angle angle) {
+        return run(() -> pivotMotor.setControl(control.withPosition(angle)));
+    }
+
+    private Command followPosition(Supplier<Angle> angle) {
+        return run(() -> pivotMotor.setControl(control.withPosition(angle.get())));
+    }
+
+    public Command pointTowardsFieldCentric(Supplier<Angle> robot_angle, Supplier<Angle> field_centric_angle) {
+        return followPosition(() ->
+            robot_angle.get().minus(field_centric_angle.get())
+        );
     }
 
     public Command pointTowardsFieldCentric(Supplier<Pose2d> robot_pose, Pose2d goal_pose) {
@@ -26,9 +51,9 @@ public class TurretPivot extends SubsystemBase {
                 goal_pose.getX() - turret_pose.getX()
             ));
 
-            return io.getPosition().plus(diff);
+            return pivotAngle.getValue().plus(diff);
         };
 
-        return run(() -> io.followPosition(point_towards_goal));
+        return run(() -> followPosition(point_towards_goal));
     }
 }

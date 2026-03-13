d5e612b7b0f492a378e127a50359834256afe499
bjcardwell <bjcardwell@pm.me>
2026-03-13 08:48:27 -0500

    fix?

--- src/main/java/frc/robot/supersystems/ShooterSupersystem.java
+++ src/main/java/frc/robot/supersystems/ShooterSupersystem.java
@@ -4,9 +4,9 @@ import static edu.wpi.first.units.Units.Degrees;
 import static edu.wpi.first.units.Units.Feet;
 import static edu.wpi.first.units.Units.Inches;
 import static edu.wpi.first.units.Units.Meter;
+import static edu.wpi.first.units.Units.RPM;
 import static edu.wpi.first.units.Units.Radians;
 
-import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.BooleanSupplier;
 import java.util.function.Supplier;
 
@@ -26,6 +26,7 @@ import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.numbers.N1;
 import edu.wpi.first.math.numbers.N2;
 import edu.wpi.first.units.measure.Angle;
+import edu.wpi.first.units.measure.AngularVelocity;
 import edu.wpi.first.units.measure.Distance;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
@@ -34,9 +35,8 @@ import frc.robot.subsystems.shooter.Shooter;
 import frc.robot.subsystems.shooterpitch.ShooterPitch;
 import frc.robot.subsystems.turret.Turret;
 import frc.robot.subsystems.turretloader.TurretLoader;
-import frc.robot.supersystems.ShotSolutionCalculator.ShotInput;
 import frc.robot.supersystems.ShotSolutionCalculator.ShotParameters;
-import frc.robot.supersystems.ShotSolutionCalculator.ShotSolution;
+import frc.robot.supersystems.ShotSolutionCalculator.TargetSelection;
 import frc.robot.subsystems.drive.Drive;
 import frc.robot.subsystems.indexer.Indexer;
 import frc.robot.subsystems.intakepivot.IntakePivot;
@@ -75,7 +75,8 @@ public class ShooterSupersystem extends SubsystemBase {
         distanceToPitchAndRPM.put(dist.in(Meter), new Matrix<N2, N1>(new SimpleMatrix(new double[] { pitchRad, rpm })));
     }
 
-    public ShooterSupersystem(Turret turretPivot, ShooterPitch turretPitch, Shooter shooter, Indexer indexer, TurretLoader turretLoader,
+    public ShooterSupersystem(Turret turretPivot, ShooterPitch turretPitch, Shooter shooter, Indexer indexer,
+            TurretLoader turretLoader,
             Drive drive, IntakePivot intakePivot) {
 
         putMeasurement(Feet.of(7), 20, 1300);
@@ -97,7 +98,7 @@ public class ShooterSupersystem extends SubsystemBase {
         this.indexer = indexer;
         this.turretLoader = turretLoader;
         this.drive = drive;
-    
+
         this.intakePivot = intakePivot;
 
         setDefaultCommand(defaultHoming());
@@ -113,32 +114,66 @@ public class ShooterSupersystem extends SubsystemBase {
         return new Pose3d(drive.getPose()).transformBy(Constants.turretOffset).getTranslation();
     }
 
-    private ShotSolution computeShotSolution() {
+    private TargetSelection getTargetSelection(Translation3d turretPosition) {
+        TargetSelection targetSelection = ShotSolutionCalculator.getTargetSelection(turretPosition);
+
+        Logger.recordOutput("Shooter/PassingShotActive", targetSelection.passingShotActive());
+        Logger.recordOutput(
+                "Shooter/AllianceRelativeTargetPosition",
+                AllianceFlipUtil.apply(targetSelection.target()).toTranslation2d());
+        return targetSelection;
+    }
+
+    private Translation3d getTarget(Translation3d turretPosition) {
+        return getTargetSelection(turretPosition).target();
+    }
+
+    private Translation3d getTarget() {
+        return getTarget(getTurretPosition());
+    }
+
+    private double estimateTimeOfFlight(double horizontalDist) {
+        ShotParameters shotParameters = getShotParameters(horizontalDist);
+        double exitVelocity = (shotParameters.rpm() * 2.0 * Math.PI * Constants.FLYWHEEL_RADIUS_METERS / 60.0)
+                * Constants.SPIN_TRANSFER_EFFICIENCY;
+
+        return horizontalDist / (exitVelocity * Math.cos(shotParameters.pitchRadians()));
+    }
+
+    private double getDistanceToTarget() {
+        double distance = getTarget().toTranslation2d().getDistance(getTurretPosition().toTranslation2d());
+        Logger.recordOutput("Shooter/DistanceToTargetMeters", distance);
+        return distance;
+    }
+
+    private Angle getLeadYaw() {
         ChassisSpeeds robotSpeeds = drive.getChassisSpeeds();
         Rotation2d heading = drive.getPose().getRotation();
         ChassisSpeeds fieldSpeeds = ChassisSpeeds.fromRobotRelativeSpeeds(robotSpeeds, heading);
         Translation2d fieldVelocity = new Translation2d(fieldSpeeds.vxMetersPerSecond, fieldSpeeds.vyMetersPerSecond);
-        ShotSolution shotSolution = ShotSolutionCalculator.solve(
-                new ShotInput(getTurretPosition(), heading, fieldVelocity),
-                this::getShotParameters);
 
-        Logger.recordOutput("Shooter/DistanceToTargetMeters", shotSolution.distanceMeters());
-        Logger.recordOutput("Shooter/PassingShotActive", shotSolution.passingShotActive());
-        Logger.recordOutput(
-                "Shooter/AllianceRelativeTargetPosition",
-                AllianceFlipUtil.apply(shotSolution.target()).toTranslation2d());
-        return shotSolution;
+        Translation3d turretPosition = getTurretPosition();
+        Translation3d target = getTarget(turretPosition);
+        double distance = target.toTranslation2d().getDistance(turretPosition.toTranslation2d());
+
+        double timeOfFlight = estimateTimeOfFlight(distance);
+        Translation2d fieldRelativeTargetVector = ShotSolutionCalculator.getFieldRelativeLeadTargetVector(
+                turretPosition.toTranslation2d(),
+                target.toTranslation2d(),
+                fieldVelocity,
+                timeOfFlight);
+        Angle turretAngle = ShotSolutionCalculator.getRobotRelativeYaw(fieldRelativeTargetVector, heading);
+        return Turret.clampAngle(turretAngle);
     }
 
-    private ShotSolution getCurrentShotSolution(
-            AtomicReference<ShotSolution> currentShotSolution,
-            Supplier<ShotSolution> shotSolutionSupplier) {
-        ShotSolution shotSolution = currentShotSolution.get();
-        if (shotSolution == null) {
-            shotSolution = shotSolutionSupplier.get();
-            currentShotSolution.set(shotSolution);
-        }
-        return shotSolution;
+    private Angle getPitchAngle() {
+        double distance = getDistanceToTarget();
+        return Radians.of(getShotParameters(distance).pitchRadians());
+    }
+
+    private AngularVelocity getShooterRPM() {
+        double distance = getDistanceToTarget();
+        return RPM.of(getShotParameters(distance).rpm());
     }
 
     public boolean isReadyToFire(Angle turretAngle, Angle pitchAngle) {
@@ -148,44 +183,46 @@ public class ShooterSupersystem extends SubsystemBase {
                 && turretLoader.atSpeed.getAsBoolean();
     }
 
-    public Command shootWhenReady(Supplier<ShotSolution> shotSolutionSupplier, BooleanSupplier fireTrigger) {
-        AtomicReference<ShotSolution> currentShotSolution = new AtomicReference<>();
-        Supplier<ShotSolution> currentSolution =
-                () -> getCurrentShotSolution(currentShotSolution, shotSolutionSupplier);
+    public Command shootWhenReady(
+            Supplier<Angle> turretAngle,
+            Supplier<Angle> pitchAngle,
+            Supplier<AngularVelocity> rpm,
+            BooleanSupplier fireTrigger) {
         BooleanSupplier shouldFeed = () -> fireTrigger.getAsBoolean()
-                && isReadyToFire(currentSolution.get().yaw(), currentSolution.get().pitch());
+                && isReadyToFire(turretAngle.get(), pitchAngle.get());
 
         return Commands.parallel(
-                run(() -> currentShotSolution.set(shotSolutionSupplier.get())),
-                turretPivot.followAngle(() -> currentSolution.get().yaw()),
-                turretPitch.followAngle(() -> currentSolution.get().pitch()),
-                shooter.runFlywheels(() -> currentSolution.get().rpm()),
+                run(() -> {
+                    Logger.recordOutput("Shooter/ShouldFeed", shouldFeed.getAsBoolean());
+                }),
+                turretPivot.followAngle(turretAngle),
+                turretPitch.followAngle(pitchAngle),
+                shooter.runFlywheels(rpm),
                 turretLoader.feed(),
                 Commands.sequence(
-                    Commands.waitUntil(shouldFeed),
-                    indexer.feed()
-                ))
-                .beforeStarting(() -> currentShotSolution.set(shotSolutionSupplier.get()));
+                        Commands.waitUntil(shouldFeed),
+                        indexer.feed()));
     }
 
     public Command shootAtTarget(BooleanSupplier fireTrigger) {
-        return shootWhenReady(this::computeShotSolution, fireTrigger);
+        return shootWhenReady(
+                this::getLeadYaw,
+                this::getPitchAngle,
+                this::getShooterRPM,
+                fireTrigger);
     }
 
     public Command defaultHoming() {
-        AtomicReference<ShotSolution> currentShotSolution = new AtomicReference<>();
-        Supplier<ShotSolution> currentSolution =
-                () -> getCurrentShotSolution(currentShotSolution, this::computeShotSolution);
         BooleanSupplier turretCanMove = () -> !staticTurretMode.get() && intakePivot.isDeployed.getAsBoolean();
         Supplier<Angle> yaw = () -> turretCanMove.getAsBoolean()
-                ? currentSolution.get().yaw()
+                ? getLeadYaw()
                 : Radians.of(turretPivot.getTurretPositionRads());
-        Supplier<Angle> pitch = () -> currentSolution.get().pitch();
+        Supplier<Angle> pitch = this::getPitchAngle;
 
         return Commands.parallel(
-            run(() -> currentShotSolution.set(computeShotSolution())),
-            turretPivot.followAngle(yaw),
-            turretPitch.followAngle(pitch)
-        ).beforeStarting(() -> currentShotSolution.set(computeShotSolution()));
+                run(() -> {
+                }),
+                turretPivot.followAngle(yaw),
+                turretPitch.followAngle(pitch));
     }
 }
